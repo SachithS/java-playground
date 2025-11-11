2c98391d0b6a0614216007ad8a3aaa96451795e3
Sachith Senarathne <sachith.senarathnes@gmail.com>
2025-11-12 00:26:08 +0800

    Adding initial commit

---
+++ src/test/java/com/sac/internal/java_playground/JavaPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sac.internal.java_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
